bb8582fa6037bd3679d7687574fddc6eec4dd9dd
Francisco Rey Zaluar <franciscoreyzaluar@gmail.com>
2018-04-08 14:24:22 +0200

    Main code and classes

---
+++ Tetris.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace Tetris_v2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // ----------- PROJET C# : Tetris ------------
+
+            // Construction du GameBoard
+            GameBoard gb = new GameBoard();
+            gb.Build();
+            gb.landed = gb.FillLanded();
+            gb.ShowLanded();
+
+            // Initialisation des tetrominos
+            // ------ I -------
+            Tetromino I = new Tetromino();
+            I.shape = new int[1, 4] { { 1, 1, 1, 1 } };
+            I.rotations = new int[2][,];
+            I.rotations[0] = new int[1, 4] {
+                {1,1,1,1}
+            };
+            I.rotations[1] = new int[4, 1]{
+                {1},
+                {1},
+                {1},
+                {1}
+            };
+            I.colour = ConsoleColor.Cyan;
+            I.landed = gb.landed;
+            // ------ J -------
+            Tetromino J = new Tetromino();
+            J.shape = new int[3, 2] {
+                {0,1},
+                {0,1},
+                {1,1}
+            };
+            J.rotations = new int[4][,];
+            J.rotations[0] = new int[3, 2]{
+                {0,1},
+                {0,1},
+                {1,1}
+            };
+            J.rotations[1] = new int[2, 3]{
+                {1,0,0},
+                {1,1,1},
+            };
+            J.rotations[2] = new int[3, 2]{
+                {1,1},
+                {1,0},
+                {1,0}
+            };
+            J.rotations[3] = new int[2, 3]{
+                {1,1,1},
+                {0,0,1},
+            };
+            J.colour = ConsoleColor.Blue;
+            J.landed = gb.landed;
+            // ------ L -------
+            Tetromino L = new Tetromino();
+            L.shape = new int[3, 2] {
+                {1,0},
+                {1,0},
+                {1,1}
+            };
+            L.rotations = new int[4][,];
+            L.rotations[0] = new int[3, 2]{
+                {1,0},
+                {1,0},
+                {1,1}
+            };
+            L.rotations[1] = new int[2, 3]{
+                {1,1,1},
+                {1,0,0}
+            };
+            L.rotations[2] = new int[3, 2]{
+                {1,1},
+                {0,1},
+                {0,1}
+            };
+            L.rotations[3] = new int[2, 3]{
+                {0,0,1},
+                {1,1,1}
+            };
+            L.colour = ConsoleColor.DarkYellow;
+            L.landed = gb.landed;
+
+            // ------ O -------
+            Tetromino O = new Tetromino();
+            O.shape = new int[2, 2]{
+                {1,1},
+                {1,1}
+            };
+            O.rotations = new int[1][,];
+            O.rotations[0] = new int[2,2]{
+                {1,1},
+                {1,1}
+            };
+            O.colour = ConsoleColor.Yellow;
+            O.landed = gb.landed;
+            // ------ S -------
+            Tetromino S = new Tetromino();
+            S.shape = new int[2, 3]{
+                {0,1,1},
+                {1,1,0}
+            };
+            S.rotations = new int[2][,];
+            S.rotations[0] = new int[2,3]{
+                {0,1,1},
+                {1,1,0}
+            };
+            S.rotations[1] = new int[3, 2]{
+                {1,0},
+                {1,1},
+                {0,1}
+            };
+            S.colour = ConsoleColor.Green;
+            S.landed = gb.landed;
+            // ------ T -------
+            Tetromino T = new Tetromino();
+            T.shape = new int[2, 3]{
+                {1,1,1},
+                {0,1,0}
+            };
+            T.rotations = new int[4][,];
+            T.rotations[0] = new int[2,3]{
+                {1,1,1},
+                {0,1,0}
+            };
+            T.rotations[1] = new int[3, 2]{
+                {0,1},
+                {1,1},
+                {0,1}
+            };
+            T.rotations[2] = new int[2, 3]{
+                {0,1,0},
+                {1,1,1}
+            };
+            T.rotations[3] = new int[3, 2]{
+                {1,0},
+                {1,1},
+                {1,0}
+            };
+            T.colour = ConsoleColor.Magenta;
+            T.landed = gb.landed;
+            // ------- Z ------
+            Tetromino Z = new Tetromino();
+            Z.shape = new int[2, 3]{
+                {1,1,0},
+                {0,1,1}
+            };
+            Z.rotations = new int[2][,];
+            Z.rotations[0] = new int[2, 3]{
+                {1,1,0},
+                {0,1,1}
+            };
+            Z.rotations[1] = new int[3, 2]{
+                {0,1},
+                {1,1},
+                {1,0}
+            };
+            Z.colour = ConsoleColor.Red;
+            Z.landed = gb.landed;
+
+            I.TopLeft = new int[2] { 6, 5 };
+            I.potentialTopLeft = I.TopLeft;
+
+            while(true)
+            {
+                I.GetInput();
+            }
+
+        }
+    }
+}
---
+++ TetrisClasses.cs
@@ -0,0 +1,235 @@
+﻿using System;
+namespace Tetris_v2
+{
+    public class GameBoard
+    {
+        // attributs
+        public int[][] landed;
+        public int[][] grid;
+
+        // méthodes
+
+        // méthode construisant les bordures
+        public void Build()
+        {
+            Console.SetWindowSize(50, 150);
+
+            // écriture du titre du jeu
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(37, 2);
+            Console.WriteLine("TETRIS");
+
+            // construction des bordures verticales
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            for (int i = 0; i < 40; i ++)
+            {
+                Console.SetCursorPosition(4, 5 + i);
+                Console.WriteLine("@");
+                Console.SetCursorPosition(5, 5 + i);
+                Console.WriteLine("@");
+                Console.SetCursorPosition(75, 5 + i);
+                Console.WriteLine("@");
+                Console.SetCursorPosition(76, 5 + i);
+                Console.WriteLine("@");
+            }
+            // construction des lignes horizontales
+            for (int i = 0; i < 73; i++)
+            {
+                Console.SetCursorPosition(4 + i, 4);
+                Console.WriteLine("@");
+                Console.SetCursorPosition(4 + i, 45);
+                Console.WriteLine("@");
+                Console.SetCursorPosition(4 + i, 46);
+                Console.WriteLine("@");
+                Console.SetCursorPosition(4 + i, 47);
+                Console.WriteLine("@");
+            }
+        }
+
+        public int[][] FillLanded()
+        {
+            int[][] landed1 = new int[40][];
+            int[] arr = new int[69];
+
+            for (int j = 0; j < 69; j++)
+            {
+                arr[j] = 0;
+            }
+
+            for (int i = 0; i < 40; i++)
+            {
+                landed1[i] = arr;    
+            }
+            return landed1;
+        }
+
+        public void ShowLanded()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (int i = 0; i < this.landed.Length; i++)
+            {
+                for (int j = 0; j < this.landed[1].Length; j++)
+                {
+                    Console.SetCursorPosition(6 + j, 5 + i);
+                    Console.WriteLine(this.landed[i][j]);
+                }
+            }
+        }
+
+        // constructeur
+        public GameBoard()
+        {
+        }
+    }
+
+    public class Tetromino
+    {
+        // attributs
+        public int[,] shape;
+        public int[] TopLeft;
+        public int[] potentialTopLeft;
+        public int[][,] rotations;
+        public ConsoleColor colour;
+        public int indicator = 0;
+        public int[][] landed;
+
+        // méthodes
+        public void Show()
+        {
+            Console.ForegroundColor = this.colour;
+            Console.SetCursorPosition(this.TopLeft[0], this.TopLeft[1]);
+
+            for (int i = 0; i <= this.rotations[this.indicator].GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= this.rotations[this.indicator].GetUpperBound(1); j++)
+                {
+                    Console.SetCursorPosition(this.TopLeft[0] + i, this.TopLeft[1] + j);
+                    Console.WriteLine(this.rotations[this.indicator][i, j]);
+                }
+
+            }
+        }
+
+        public void Clear()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i <= this.rotations[this.indicator].GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= this.rotations[this.indicator].GetUpperBound(1); j++)
+                {
+                    Console.SetCursorPosition(this.TopLeft[0] + i, this.TopLeft[1] + j);
+                    Console.WriteLine(0);
+                }
+            }
+        }
+
+        public bool CanIShow()
+        {
+            bool show = true;
+
+            for (int i = 0; i <= this.rotations[this.indicator].GetUpperBound(0); i++) // boucle sur les colonnes
+            {
+                for (int j = 0; j <= this.rotations[this.indicator].GetUpperBound(1); j++) // boucle sur les lignes
+                {
+                    int impact = 0;
+                    try
+                    {
+                        impact = this.landed[this.potentialTopLeft[0] + j][this.potentialTopLeft[1] + i];    // cas où on ne sort pas de la grille
+                    }
+                    catch(IndexOutOfRangeException)
+                    {
+                        impact = 1; // cas où on sort de la grille
+                    }
+
+
+                    if ((this.rotations[this.indicator][i,j] == 1) && (impact == 1))
+                    {
+                        show = false;
+                    }
+                }
+            }
+
+            return show;
+        }
+
+
+        public void GetInput()
+        {
+            ConsoleKey input = Console.ReadKey().Key;
+
+            if (input == ConsoleKey.Spacebar)
+            {
+                // afficher la rotation si possible
+                if (this.indicator + 1 < this.rotations.Length)
+                {
+                    this.indicator = this.indicator + 1;
+                    this.potentialTopLeft = this.TopLeft;
+                    if (this.CanIShow())
+                    {
+                        this.Clear();
+                        this.Show();
+                    }
+                }
+                else
+                {
+                    this.indicator = 0;
+                    this.potentialTopLeft = this.TopLeft;
+                    if (this.CanIShow())
+                    {
+						this.Clear();
+                        this.Show();
+                    }
+                }
+            }
+            if (input == ConsoleKey.RightArrow)
+            {  
+                // afficher le tetromino à la colonne de droite si possible
+                if (this.TopLeft[0] + 1 < 75)
+                {
+                    this.potentialTopLeft[0] = this.TopLeft[0] + 1;
+                    if (this.CanIShow())
+                    {
+                        this.Clear();
+                        this.TopLeft = this.potentialTopLeft;
+                        this.Show();
+                    }
+                }
+            }
+            if (input == ConsoleKey.LeftArrow)
+            {
+                // afficher le tetromino à la colonne de gauche si possible
+                if (this.TopLeft[0] - 1 > 5)
+                {
+                    this.potentialTopLeft[0] = this.TopLeft[0] - 1;
+                    if (this.CanIShow())
+                    {
+                        this.Clear();
+                        this.TopLeft = this.potentialTopLeft;
+                        this.Show();
+                    }
+                }
+            }
+            if (input == ConsoleKey.DownArrow)
+            {
+                // accélerer la descente
+                if (this.TopLeft[1] + 1 < 45)
+                {
+                    this.potentialTopLeft[1] = this.TopLeft[1] + 1;
+                    if (this.CanIShow()) 
+                    {
+						this.Clear();
+                        this.TopLeft = this.potentialTopLeft;
+                        this.Show();
+                    }
+                }
+            }
+        }
+
+        // constructeurs
+        public Tetromino()
+        {
+                    
+        }
+    }
+}
